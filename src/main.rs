41898dd287e30a25877f7f79f8f0940c0f43df71
benjamin <benjamin@leveritt.co.uk>
2021-05-24 09:20:13 +0100

    Moves generation to GPT-Neo

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,51 @@
 extern crate anyhow;
 
+use rust_bert::gpt_neo::{
+    GptNeoConfigResources, GptNeoMergesResources, GptNeoModelResources, GptNeoVocabResources,
+};
 use rust_bert::pipelines::common::ModelType;
 use rust_bert::pipelines::text_generation::{TextGenerationConfig, TextGenerationModel};
+use rust_bert::resources::{RemoteResource, Resource};
+use tch::Device;
 
 fn main() -> anyhow::Result<()> {
-    //    Set-up masked LM model
+    //    Set-up model resources
+    let config_resource = Resource::Remote(RemoteResource::from_pretrained(
+        GptNeoConfigResources::GPT_NEO_1_3B,
+    ));
+    let vocab_resource = Resource::Remote(RemoteResource::from_pretrained(
+        GptNeoVocabResources::GPT_NEO_1_3B,
+    ));
+    let merges_resource = Resource::Remote(RemoteResource::from_pretrained(
+        GptNeoMergesResources::GPT_NEO_1_3B,
+    ));
+    let model_resource = Resource::Remote(RemoteResource::from_pretrained(
+        GptNeoModelResources::GPT_NEO_1_3B,
+    ));
     let generate_config = TextGenerationConfig {
-        model_type: ModelType::GPT2,
-        max_length: 30,
-        do_sample: true,
-        num_beams: 5,
-        temperature: 1.1,
+        model_type: ModelType::GPTNeo,
+        model_resource,
+        config_resource,
+        vocab_resource,
+        merges_resource,
+        min_length: 10,
+        max_length: 32,
+        do_sample: false,
+        early_stopping: true,
+        num_beams: 4,
         num_return_sequences: 1,
+        device: Device::Cpu,
         ..Default::default()
     };
+
     let model = TextGenerationModel::new(generate_config)?;
 
-    let input_context = "The dog can";
-    let second_input_context = "The cat was";
-    let output = model.generate(&[input_context, second_input_context], None);
+    let input_context_1 = "It was a very nice and sunny";
+    let input_context_2 = "It was a gloom winter night, and";
+    let output = model.generate(&[input_context_1, input_context_2], None);
 
     for sentence in output {
-        println!("{:?}", sentence);
+        println!("{}", sentence);
     }
     Ok(())
 }
